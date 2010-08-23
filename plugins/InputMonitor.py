94c1d73cb6f5cabc098dabd53c065bd5028d889c
Adam Sutton <aps@hp468>
2010-08-23 22:01:57 +0100

    Started work on removing static /dev/input requirement. I have defined function to load dir contents, but still need to add signal handler for change events.

--- plugins/InputMonitor.py
+++ plugins/InputMonitor.py
@@ -18,6 +18,8 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os, re, threading
+import select
+from fcntl import fcntl, F_NOTIFY, DN_CREATE, DN_DELETE, DN_MULTISHOT
 from logging import error, debug, info, warn
 from Monitor import Monitor
 
@@ -43,6 +45,56 @@ class InputMonitor ( Monitor, threading.Thread ):
             self._path = config['path']
         if not config.has_key('name'): self._name = self._path
 
+        # Register for directory events / setup input watches
+        self._inputs = {}
+        self._poll   = select.poll()
+        self._update_inputs()
+        self._dd     = os.open(self._path, 0)
+        fcntl(self._dd, F_NOTIFY, DN_DELETE | DN_CREATE | DN_MULTISHOT)
+
+    # Update the input events list
+    def _update_inputs ( self ):
+        events = {}
+        for p in os.listdir(self._path):
+            path = os.path.abspath(os.path.join(self._path, p))
+
+            # Ignore dirs
+            if os.path.isdir(path): continue
+
+            # Attempt to ignore already included
+            if os.path.islink(path):
+                path = os.path.abspath(os.readlink(path))
+                if path in events: continue
+
+            # Search expressions
+            res = not self._regex
+            for r in self._regex:
+                res = r.match(p)
+                if res: break
+            if not res: continue
+
+            # Existing
+            if path in self._inputs:
+                events[path] = self._inputs
+    
+            # New
+            else:
+                fp = open(path)
+                events[path] = fp
+                self._poll.register(fp.fileno(), select.POLLIN|select.POLLPRI)
+                debug('%s - adding input device %s' % (self, path))
+
+        # Remove
+        for path in self._inputs:
+            if not path in events:
+                debug('%s - removing input device %s' % (self, path))
+                fp = self._inputs[path]
+                self._poll.unregister(fp.fileno())
+                fp.close()
+
+        # Update
+        self._inputs = events
+
     # Start the thread
     def start ( self ):
         self._running = True
@@ -52,47 +104,24 @@ class InputMonitor ( Monitor, threading.Thread ):
     def stop ( self ): self._running = False
 
     # Monitor /dev/input
-    #   Note: it is assumed that /dev/input is static once powernapd starts
-    #         i.e. if someone plugs a hot-pluggable input device (such as a 
-    #              USB keyboard/mouse) this will not be detected (unless
-    #              present when this process starts
-    #   TODO: remove the requirement for static /dev/input
     def run ( self ):
-        import select
-
-        # Get all events
-        poll = select.poll()
-        fps  = {}
-        for f in os.listdir(self._path):
-            path = os.path.join(self._path, f)
-            if not os.path.isdir(path):
-                
-                # Check each regex
-                res = self._regex is None
-                if not res:
-                    for r in self._regex:
-                        res = r.match(f)
-                        if res: break
-            
-                # Include
-                if res:
-                    fp = open(path)
-                    fps[fp.fileno()] = fp
-                    poll.register(fp, select.POLLIN)
-                    debug('%s - adding input device %s' % (self, path))
 
         # Poll for events
         while self._running:
-            res = poll.poll(1000)
+            res = self._poll.poll(1000)
             if ( res ):
                 for fd, e in res:
-                    if e & select.POLLIN:
-                        debug('%s - input on %s' % (self, fps[fd].name)) 
+                    if e & (select.POLLIN|select.POLLPRI):
                         os.read(fd, 32768) # Read what is there!
                         self.reset()
-
-        # Close the files
-        for fd in fps: fps[fd].close()
+        
+                        # Debug
+                        path = None
+                        for p in self._inputs:
+                            if self._inputs[p].fileno() == fd:
+                                path = p
+                                break
+                        debug('%s - input on %s' % (self, path)) 
 
 # ###########################################################################
 # Editor directives
